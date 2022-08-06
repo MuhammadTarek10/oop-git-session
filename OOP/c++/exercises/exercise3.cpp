b6dcbccc00936c0ff3a57f2a5d37b1f7a434ea9e
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-08-06 20:11:34 +0200

    added blank files for exercises, added questions in the notebook

---
+++ OOP/c++/exercises/exercise3.cpp
@@ -0,0 +1,6 @@
+/*
+    3. Write a program with a mother class and an inherited daugther class.
+    Both of them should have a method void display ()
+    that prints a message (different for mother and daugther).In the main define a daughter and call the display() method
+    on it.
+*/
